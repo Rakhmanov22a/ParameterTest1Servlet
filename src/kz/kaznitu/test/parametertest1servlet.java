22d741603b501be685f9ea0052fad9f46cb2ce3f
Adilet <rahmanov-adilet@mail.ru>
2017-10-16 17:28:22 +0600

    getParameter мен жумыс жасау

---
+++ src/kz/kaznitu/test/parametertest1servlet.java
@@ -0,0 +1,34 @@
+package kz.kaznitu.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class parametertest1servlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+     resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        String title = "Using GET Methot to Read Form Data";
+        String docType =
+                "<!doctype html>\n";
+
+        out.println(docType +
+            "<html>\n"+
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n" +
+            "<h1 align =\"center\">" + title + "</h1>\n" +
+            "<ul>\n" +
+                " <li><b>First Name</b>: "
+                +req.getParameter("first_name") + "\n" +
+                " <li><b>Last Name</b>: "
+                +req.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>"
+        );
+    }
+}
